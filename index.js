fa31fcca12c22c07ac2a769270a744daf3cab882
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2022-01-09 12:39:26 -0500

    Finishes the Columns closing #2

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ addRow.addEventListener("click", () =>{
     const newRow = document.createElement("div")
     newRow.classList.add("row")
     NumofCols = document.querySelector(".row").querySelectorAll(".box")
-    for (let i = 0; i < NumofCols.length; i++) {
+    for (let i = 0; i < NumofCols.length; i++) { // This is to check to see some many cols are needed
         const col = document.createElement("div");
         col.classList.add("col-sm", "border", "border-dark", "box", "white");
-        newRow.appendChild(col);
+        newRow.appendChild(col); // Adds cols to the row
       }
-    getRow.appendChild(newRow)
+    getRow.appendChild(newRow) // adds the new row
+})
+const addCol = document.querySelector(".add-col")
+addCol.addEventListener("click", () =>{
+    
+    for(let i = 0; i < getRow.children.length; i++){ // Loops thorught the amount of rows
+    col = document.createElement("div");
+    col.classList.add("col-sm", "border", "border-dark", "box", "white");
+    getRow.children[i].appendChild(col) // adds a child to each div
+    }
 })
